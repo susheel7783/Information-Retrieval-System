ed52d8a9b1931275cde38a9dde66cdbdddfd1954
susheel kumar <susheelkumar7783@gmail.com>
2025-02-09 14:58:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Information Retrieval System',
+    version='0.0.0',
+    author='Susheel',
+    author_email='susheelkumar7783@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
